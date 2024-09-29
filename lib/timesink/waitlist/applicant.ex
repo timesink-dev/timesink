0a49de6a09f26a2b306e2a7afe46e3f3861b52f6
Aaron Zomback <aaronzomback@gmail.com>
2024-09-30 00:42:52 +0200

    feat: generate applicant schema and waitlist migration

---
+++ lib/timesink/waitlist/applicant.ex
@@ -0,0 +1,31 @@
+defmodule Timesink.Waitlist.Applicant do
+  use Ecto.Schema
+  use SwissSchema, repo: Timesink.Repo
+  import Ecto.Changeset
+
+  @type t :: %{
+          __struct__: __MODULE__,
+          email: String.t(),
+          status: atom()
+        }
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+
+  schema "waitlist" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :status, Ecto.Enum, values: [:pending, :invited, :completed], default: :pending
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @spec changeset(applicant :: t(), params :: %{optional(key :: atom()) => value :: term()}) ::
+          Ecto.Changeset.t()
+  def changeset(%{__struct_: __MODULE__} = struct, params) do
+    struct
+    |> cast(params, [:email, :status, :first_name, :last_name])
+    |> validate_required([:email, :first_name, :last_name])
+    |> validate_format(:email, ~r/@/)
+  end
+end
