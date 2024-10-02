3487f7784dc46517293ca9bb188dee2adc8b07ad
Aaron Zomback <aaronzomback@gmail.com>
2024-10-02 22:16:37 +0200

    feat: create join/1 method

--- lib/timesink/waitlist.ex
+++ lib/timesink/waitlist.ex
@@ -2,4 +2,25 @@ defmodule Timesink.Waitlist do
   @moduledoc """
   The Waitlist context.
   """
+  alias Timesink.Waitlist.Applicant
+  alias Timesink.Repo
+
+  @doc """
+  Creates a new applicant and adds them to the waitlist.
+
+  ## Examples
+
+      iex> join(%{first_name: "Jose", last_name: "Val Del Omar", email: "valdelomar@gmail.com"})
+      {:ok, %Timesink.Waitlist.Applicant{…}}
+
+
+      iex > join(%{first_name: "Jose", last_name: "Val Del Omar", email: "josevaldelomar"})
+      {:error, %Ecto.Changeset{…}}
+  """
+
+  def join(params) do
+    %Applicant{}
+    |> Applicant.changeset(params)
+    |> Repo.insert()
+  end
 end
