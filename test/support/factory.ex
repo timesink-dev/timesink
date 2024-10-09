24d68ee4b521e80bd25471e671750b4831f212ae
Joel Jucá <joeljuca@proton.me>
2024-10-09 10:41:52 -0300

    chore: define type `status()` on `Waitlist.Applicant`

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,7 +5,8 @@ defmodule Timesink.Factory do
     %Timesink.Waitlist.Applicant{
       first_name: Faker.Person.first_name(),
       last_name: Faker.Person.last_name(),
-      email: Faker.Internet.email()
+      email: Faker.Internet.email(),
+      status: Timesink.Waitlist.Applicant.statuses() |> Enum.random()
     }
   end
 
