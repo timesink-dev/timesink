6c1c7b7717f503aa06dd059254bbec6718b3ed39
Aaron Zomback <aaronzomback@gmail.com>
2024-11-05 00:27:53 +0100

    refactor: remove converting into atom-key maps pipe

--- lib/timesink/waitlist.ex
+++ lib/timesink/waitlist.ex
@@ -2,6 +2,7 @@ defmodule Timesink.Waitlist do
   @moduledoc """
   The Waitlist context.
   """
+
   import Ecto.Changeset
   alias Timesink.Waitlist.Applicant
 
@@ -17,20 +18,9 @@ defmodule Timesink.Waitlist do
           {:ok, Applicant.t()} | {:error, Ecto.Changeset.t()}
 
   def join(params) do
-    params_schema = %{
-      first_name: :string,
-      last_name: :string,
-      email: :string
-    }
-
-    changeset =
-      {%Applicant{}, params_schema}
-      |> cast(params, Map.keys(params_schema))
-      |> validate_required([:first_name, :last_name, :email])
-
-    with {:ok, params} <- apply_action(changeset, :join),
-         {:ok, user} <- Applicant.create(params) do
-      {:ok, user}
+    with {:ok, applicant} <-
+           Applicant.create(params) do
+      {:ok, applicant}
     end
   end
 end
