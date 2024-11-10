7b4a53dac7dcf0035eba31528b93247145bca769
Aaron Zomback <aaronzomback@gmail.com>
2024-11-10 14:18:33 +0100

    feat: set timeout to clear flash toasts (#13)

---
+++ assets/js/hooks.js
@@ -0,0 +1,12 @@
+const Hooks = {};
+
+Hooks.HideFlash = {
+  mounted() {
+    setTimeout(() => {
+      this.pushEvent("lv:clear-flash", { key: this.el.dataset.key });
+      this.el.style.display = "none";
+    }, 3000);
+  },
+};
+
+export default Hooks;
