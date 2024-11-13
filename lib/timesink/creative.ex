893eacc8cea0cf0311f789b9216d339d11de9f2c
Joel Jucá <joeljuca@proton.me>
2024-11-13 20:08:27 -0300

    WIP on schema design

---
+++ lib/timesink/creative.ex
@@ -0,0 +1,34 @@
+defmodule Timesink.Creative do
+  use Ecto.Schema
+  use SwissSchema, repo: Timesink.Repo
+  import Ecto.Changeset
+
+  @type t :: %{
+          __struct__: __MODULE__,
+          first_name: :string,
+          last_name: :string,
+          user: Timesink.Accounts.User.t()
+        }
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "creatives" do
+    belongs_to :user, Timesink.Accounts.User
+
+    field :first_name, :string
+    field :last_name, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @spec changeset(creative :: t(), params :: %{optional(atom()) => term()}) ::
+          Ecto.Changeset.t()
+  def changeset(%{__struct__: __MODULE__} = struct, %{} = params) do
+    struct
+    |> cast(params, [:first_name, :last_name])
+    |> validate_required([:first_name, :last_name])
+    |> cast_assoc(:user, with: &Timesink.Accounts.User.changeset/2)
+    |> validate_length(:first_name, min: 2)
+    |> validate_length(:last_name, min: 2)
+  end
+end
---
+++ lib/timesink/film.ex
@@ -0,0 +1,48 @@
+defmodule Timesink.Film do
+  use Ecto.Schema
+  use SwissSchema, repo: Timesink.Repo
+  import Ecto.Changeset
+
+  @type color :: :black_and_white | :sepia | :monochrome | :partially_colorized | :color
+  @colors [:black_and_white, :sepia, :monochrome, :partially_colorized, :color]
+
+  @type t :: %{
+          __struct__: __MODULE__,
+          title: :string,
+          year: :integer,
+          duration: :integer,
+          color: color(),
+          aspect_ratio: :string,
+          format: :integer,
+          synopsis: :string
+        }
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "films" do
+    field :title, :string
+    field :year, :integer
+    field :duration, :integer
+    field :color, Ecto.Enum, values: @colors
+    field :aspect_ratio, :string
+    field :format, :integer
+    field :synopsis, :string
+
+    # has_many :directors, Creative
+    # has_many :producers, Creative
+    # has_many :writers, Creative
+    # has_many :cast, Creative
+    # has_many :crew, Creative
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @spec changeset(film :: t(), params :: %{optional(atom()) => term()}) ::
+          Ecto.Changeset.t()
+  def changeset(%{__struct__: __MODULE__} = struct, %{} = params) do
+    struct
+    |> cast(params, [:title, :year, :duration, :color, :aspect_ratio, :format, :synopsis])
+    |> validate_required([:title, :year, :synopsis])
+    |> validate_length(:title, min: 1)
+  end
+end
