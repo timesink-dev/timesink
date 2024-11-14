f5ef9564e7e279f3e95fa5f57672b18328c02337
Joel Jucá <joeljuca@proton.me>
2024-11-13 21:31:00 -0300

    build: remove `test/support` from dev env

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Timesink.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(env) when env in [:dev, :test], do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
