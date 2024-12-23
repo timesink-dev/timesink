3eb8b2e2afd5f75e147008b689cd0486aed5354c
Aaron Zomback <aaronzomback@gmail.com>
2024-12-23 01:00:16 +0100

    chore: update mock user desc

--- lib/timesink/account/accounts.ex
+++ lib/timesink/account/accounts.ex
@@ -38,6 +38,7 @@ defmodule Timesink.Accounts do
   end
 
   # Mock implementation for development
+  # TEMP: enter the user id you want to mock from your dev database
   @mock_current_user_id ~c"5ca3328d-2e94-4bec-80a4-90595bc98d5b"
 
   def get_user_by!(fields), do: User.get_by(fields)
