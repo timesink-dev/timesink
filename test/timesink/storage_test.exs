cbfb26b100ede539a38314c669a259b65f4186ba
Joel Jucá <joeljuca@proton.me>
2025-02-06 18:20:53 -0300

    test: add tests for `Storage.create_attachment/3`

--- test/timesink/storage_test.exs
+++ test/timesink/storage_test.exs
@@ -3,6 +3,7 @@ defmodule Timesink.StorageTest do
   import Timesink.Factory
   alias Timesink.Storage
   alias Timesink.Storage.Blob
+  alias Timesink.Storage.Attachment
   alias ExAws.S3
 
   describe "create_blob/2" do
@@ -34,6 +35,21 @@ defmodule Timesink.StorageTest do
 
     test "accept an opt `user_id`", %{user: %{id: uid}, upload: upload} do
       assert {:ok, %{user_id: ^uid}} = Storage.create_blob(upload, user_id: uid)
+    end
+  end
+
+  describe "create_attachment/3" do
+    setup ctx do
+      %{
+        upload: build(:plug_upload)
+      }
+      |> Map.merge(ctx)
+    end
+
+    test "creates an attachment out of a %Plug.Upload{}", %{upload: upload} do
+      params = params_for(:attachment) |> Map.take([:target_schema, :target_id, :name])
+
+      assert {:ok, %Attachment{}} = Storage.create_attachment(upload, params)
     end
   end
 end
