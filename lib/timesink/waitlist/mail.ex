b1aae585522572ce33410003c7df101f931d3062
Aaron Zomback <aaronzomback@gmail.com>
2025-03-06 01:22:05 +0100

    feat: init send invite worker

--- lib/timesink/waitlist/mail.ex
+++ lib/timesink/waitlist/mail.ex
@@ -1,6 +1,8 @@
 defmodule Timesink.Waitlist.Mail do
   use Timesink.Mailer
 
+  @base_url "https://timesinkpresents.com"
+
   def send_waitlist_confirmation(to_email, first_name) do
     send_mail(
       to_email,
@@ -9,6 +11,20 @@ defmodule Timesink.Waitlist.Mail do
       Hey #{first_name},
 
       You're on the waitlist! We'll let you know when you are the next one in line to join.
+      """
+    )
+  end
+
+  def send_invite_code(to_email, first_name, code) do
+    send_mail(
+      to_email,
+      "You're invited to join!",
+      """
+      Hey #{first_name},
+
+      You're invited to join! Click the link below to create your account:
+
+      #{@base_url}/invite/#{code}
       """
     )
   end
---
+++ lib/timesink/waitlist/workers/send_invite.ex
@@ -0,0 +1,36 @@
+defmodule TimeSink.Workers.SendInvite do
+  use Oban.Worker, queue: :waitlist_invites, max_attempts: 3
+
+  alias Timesink.Waitlist.Applicant
+  alias Timesink.Token
+  alias Timesink.Waitlist.Mail
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"applicant_id" => applicant_id}}) do
+    with {:ok, applicant} = Applicant.get!(applicant_id) do
+      send_invite(applicant)
+    end
+  end
+
+  defp send_invite(%Applicant{id: applicant_id, email: email, first_name: first_name}) do
+    with {:ok, token} <- generate_and_store_token(applicant_id),
+         {:ok, _} <- Mail.send_invite_code(email, first_name, token.secret) do
+      {:ok, token}
+    end
+  end
+
+  defp generate_and_store_token(applicant_id) do
+    token = Ecto.UUID.generate()
+
+    token_params = %{
+      kind: :invite,
+      secret: token,
+      applicant_id: applicant_id
+    }
+
+    with {:ok, token} <-
+           Token.create(token_params) do
+      {:ok, token}
+    end
+  end
+end
