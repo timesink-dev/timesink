0e8d7fa925c5d07290adca404649b0390061f197
Aaron Zomback <aaronzomback@gmail.com>
2025-06-04 05:39:22 +0200

    chore: remove io inspect

--- test/timesink_web/controllers/page_controller_test.exs
+++ test/timesink_web/controllers/page_controller_test.exs
@@ -3,7 +3,6 @@ defmodule TimesinkWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    IO.inspect(html_response(conn, 200), label: "hello")
     assert html_response(conn, 200) =~ "TimeSink Presents"
   end
 end
