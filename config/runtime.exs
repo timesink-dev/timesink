0ecf7f7b20ced76d0042d609beda26ef4006a258
Aaron Zomback <aaronzomback@gmail.com>
2025-07-31 11:20:54 +0200

    chore: add check origin for endpoint liveview connection

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,6 +57,10 @@ if config_env() == :prod do
 
   config :timesink, TimesinkWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: [
+      "https://timesink-staging.fly.dev",
+      "https://staging.timesinkpresents.com"
+    ],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
