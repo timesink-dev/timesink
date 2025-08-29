35216fcb7890059c45c09c289abef19a2b69b523
Aaron Zomback <aaronzomback@gmail.com>
2025-08-30 00:31:27 +0200

    feat: display online platform presence as 'lobby pill'

--- lib/timesink_web/channels/topics.ex
+++ lib/timesink_web/channels/topics.ex
@@ -2,4 +2,5 @@ defmodule TimesinkWeb.PubSubTopics do
   def scheduler_topic(theater_id), do: "scheduler:theater:#{theater_id}"
   def presence_topic(theater_id), do: "presence:theater:#{theater_id}"
   def phase_change_topic(theater_id), do: "phase_change:theater:#{theater_id}"
+  def platform_presence_topic, do: "presence:platform"
 end
