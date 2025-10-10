1ffdeb4e3a98b9dcdabba2accf921cdd540050cd
Aaron Zomback <aaronzomback@gmail.com>
2025-10-10 22:31:21 +0200

    chore: remove profile update topic

--- lib/timesink_web/channels/topics.ex
+++ lib/timesink_web/channels/topics.ex
@@ -2,5 +2,4 @@ defmodule TimesinkWeb.PubSubTopics do
   def scheduler_topic(theater_id), do: "scheduler:theater:#{theater_id}"
   def presence_topic(theater_id), do: "presence:theater:#{theater_id}"
   def phase_change_topic(theater_id), do: "phase_change:theater:#{theater_id}"
-  def profile_update_topic(user_id), do: "user:#{user_id}:profile"
 end
