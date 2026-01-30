32108ed7c3327ce2685c78b4be9ea7ff4d2b2360
Aaron Zomback <aaronzomback@gmail.com>
2026-01-30 11:52:13 +0100

    chore: disable canonical url redirects in test env

--- lib/timesink_web/plugs/canonical_host.ex
+++ lib/timesink_web/plugs/canonical_host.ex
@@ -4,27 +4,28 @@ defmodule TimesinkWeb.Plugs.CanonicalHost do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    canonical_host = System.get_env("PHX_HOST")
+    if Application.get_env(:timesink, :canonical_host_redirect, true) do
+      canonical_host = System.get_env("PHX_HOST")
 
-    cond do
-      # No PHX_HOST → dev / test / local → do nothing
-      is_nil(canonical_host) ->
-        conn
+      cond do
+        is_nil(canonical_host) ->
+          conn
 
-      # Already on canonical host → do nothing
-      conn.host == canonical_host ->
-        conn
+        conn.host == canonical_host ->
+          conn
 
-      # Redirect everything else → canonical
-      true ->
-        location =
-          "https://#{canonical_host}#{conn.request_path}" <>
-            if(conn.query_string != "", do: "?" <> conn.query_string, else: "")
+        true ->
+          location =
+            "https://#{canonical_host}#{conn.request_path}" <>
+              if(conn.query_string != "", do: "?" <> conn.query_string, else: "")
 
-        conn
-        |> put_resp_header("location", location)
-        |> send_resp(301, "")
-        |> halt()
+          conn
+          |> put_resp_header("location", location)
+          |> send_resp(301, "")
+          |> halt()
+      end
+    else
+      conn
     end
   end
 end
